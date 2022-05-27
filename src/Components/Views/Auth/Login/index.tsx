7cb9fb14296f49cc79959609ff19d66479c75d8c
allnullable <garrixdit@gmail.com>
2022-05-27 15:59:06 +0300

    Writing login UI

--- src/Components/Views/Auth/Login/index.tsx
+++ src/Components/Views/Auth/Login/index.tsx
@@ -16,7 +16,9 @@ import { FormikDefaultInput } from "../../../Commons/Inputs/FormikDefaultInput";
 import { gapi } from "gapi-script";
 import { DefaultButton } from "../../../Commons/DefaultButton";
 
-var background1 = require("../../../../Assets/Background1.png");
+import "./styles.scss";
+
+const logo = require('../../../../Assets/Logo.png');
 
 export const Login: React.FC = () => {
   const { loginByEmailUser, externalLoginlUser } = useActions();
@@ -76,15 +78,14 @@ export const Login: React.FC = () => {
 
   return (
     <div className="overflow-x-hidden w-full min-h-screen bg-gradient-to-b from-dark-100 to-dark-200 grid grid-cols-12 items-center relative">
-      <div className="absolute w-full overflow-hidden bg-dark-200 h-screen grid grid-cols-12 grid-row-2 items-center">
-        <div className="h-1/2 w-full col-span-8 bg-blue-400"></div>
-        <div className="h-1/2 w-full col-span-4"></div>
-        <div className="h-1/3 w-full col-span-1 bg-yellow-400"></div>
-         {/* <img alt="background" src={background1} className="h-1/4 scale-110 bg-cover col-span-8" /> */}
-        {/* <img alt="background" src={background1} className="p-2 w-3/4 h-1/4 ml-auto" /> */}
+      <div className="absolute w-full overflow-hidden grid grid-cols-12 grid-row-3 items-center">
+        <img alt="logo" src={logo} className="z-10 col-start-6 col-span-4" width={350} height={350} />
+        <div className="w-full h-96 loginbackground overflow-hidden col-span-8 rounded-tr-3xl row-start-2"></div>
+        <div className="w-full h-80 loginbackground loginbackground2 overflow-hidden col-start-5 rounded-tr-3xl row-start-3 col-span-8"></div>
       </div>
-      <div className="flex flex-col justify-center items-center py-10 px-20 border-4 rounded-2xl text-white shadow-lg col-span-4 col-start-5 relative">
-        {load ?  <div className="absolute bg-black p-2 w-full h-full z-10 flex justify-center items-center opacity-30 transition-all rounded-xl"></div> : null}    
+      <div className="flex flex-col justify-center py-16 border-4 shadow-xl rounded-2xl text-white shadow-lg col-span-4 col-start-5 relative">
+        <div className="absolute w-full h-full bg-gradient-to-r from-blue-400 to-indigo-400 rounded-xl p-2 opacity-98"></div>
+        {load ?  <div className="absolute bg-black p-2 w-full h-full z-20 flex justify-center items-center opacity-30 transition-all rounded-xl"></div> : null}    
         {load ? <div className="flex flex-col gap-2 animate-spin absolute z-20">
                 <div className="flex gap-2">
                   <div className="bg-primary-100 p-3 rounded-md"></div>
@@ -95,44 +96,58 @@ export const Login: React.FC = () => {
                   <div className="bg-primary-100 p-3 rounded-md"></div>
                 </div>
               </div> : null}
-        <h1 className="font-bold text-3xl">Log in</h1>
-            <Formik
-            initialValues={initialValues}
-            validationSchema={emailLoginValidate}
-            onSubmit={onHandleSubmit}>
-            <Form>
-              <div className="flex flex-col gap-6">
-                <div className="flex flex-col gap-3 items-center">              
-                  <div className="mt-4">
-                    <h1 className="text-red-500 font-semibold text-lg">{error}</h1>
+            <div className="w-full flex flex-col px-32">        
+              <h1 className="font-bold text-3xl z-10 text-center">Log in</h1>
+              <div className="z-10 flex flex-col gap-6">
+              <Formik
+              initialValues={initialValues}
+              validationSchema={emailLoginValidate}
+              onSubmit={onHandleSubmit}>
+              <Form>
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col gap-3 items-center">              
+                    <div className="mt-4">
+                      <h1 className="text-red-500 font-semibold text-lg">{error}</h1>
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-col gap-3">
-                  <FormikDefaultInput label="email" name="email" type="email" />
-                  <FormikDefaultInput
-                    label="password"
-                    name="password"
-                    type="password"
-                  />
-                </div>
-                <DefaultButton text="Login" onClick={() => {console.log("Login")} }/>
-                <div className="flex justify-between">
-                  <div className="flex gap-1">
-                  <input
-                    type="checkbox"
-                    className="checked:bg-primary-100"
-                    defaultChecked={remember}
-                    onClick={() => {
-                      setRemember(!remember);
-                    }}                
-                  />
-                  <h1>Remember me</h1>
+                  <div className="flex flex-col gap-3">
+                    <FormikDefaultInput label="email" name="email" type="email" />
+                    <FormikDefaultInput
+                      label="password"
+                      name="password"
+                      type="password"
+                    />
                   </div>
-                  <button>Forgot password</button>
+                  <DefaultButton text="Login" onClick={() => {console.log("Login")} }/>
                 </div>
+              </Form>
+              </Formik>
+              <div className="flex justify-between">
+                        <div className="flex gap-1 justify-center items-center">
+                          <input
+                            type="checkbox"
+                            className="checked:bg-primary-100 w-5 h-5 border-0 outline-hidden"
+                            defaultChecked={remember}
+                            onClick={() => {
+                              setRemember(!remember);
+                            }}                
+                          />
+                          <h1>Remember me</h1>
+                        </div>
+                        <button onClick={() => {console.log("first")}}>Forgot password</button>
               </div>
-            </Form>
-          </Formik>
+              <div className="flex justify-center gap-3">
+              <div className="flex flex-col gap-4 w-full">
+              <hr className="w-full" />
+              <GoogleLogin
+                  clientId="62751843627-3hvrb4vhojmd60im3q708b1usgoob3ka.apps.googleusercontent.com"
+                  onSuccess={responseGoogle}
+                  onFailure={responseError}
+                  cookiePolicy={'single_host_origin'}><h1 className="font-semibold text-md">Sign in with Google</h1></GoogleLogin>
+              </div>
+              </div>
+              </div>
+            </div>
       </div>
     </div>
   );
